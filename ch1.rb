fd275f154ebcf19593706823fc52fd27067a8e61
emmilco <elliot.milco@gmail.com>
2018-04-02 22:50:48 -0400

    add implementations through page 120

--- ch1.rb
+++ ch1.rb
@@ -6,7 +6,7 @@ def binary_search(arr, target, &comp)
   hi = arr.length - 1
   while lo <= hi
     mid = (lo + hi) / 2
-    case comp(arr[mid], target)
+    case comp.call(arr[mid], target)
     when 0
       return mid
     when -1
@@ -16,4 +16,54 @@ def binary_search(arr, target, &comp)
     end
   end
   -1
+end
+
+# coin toss simulator (page 70)
+
+def flips(n)
+  heads = 0
+  tails = 0
+  n.times { rand(2).odd? ? heads+= 1 : tails += 1 }
+  puts "#{heads} heads"
+  puts "#{tails} tails"
+  puts "#{(tails - heads).abs} delta"
+end
+
+# die roll simulator (page 72)
+
+def rolls(n)
+  results = Array.new(6, 0)
+  n.times { results[rand(6)] += 1}
+  results.each.with_index { |x,i| puts "#{x} #{i+1}'s"}
+end
+
+
+# palindrome checker (page 81)
+
+def palindrome?(str)
+  (str.length / 2).times do |i|
+    return false if str[i] != str[-1 - i]
+  end
+  true
+end
+
+# counter class (page 85)
+
+class Counter
+  def initialize(name)
+    @name = name
+    @count = 0
+  end
+
+  def increment
+    @count += 1
+  end
+
+  def tally
+    @count
+  end
+
+  def to_s
+    "#{@count} #{@name}"
+  end
 end
